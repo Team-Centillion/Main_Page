41408c3077146f3b3c99b068bb94ab468a785b82
james1112 <jiwon1112kim@naver.com>
2022-04-12 16:35:36 +0900

    엄청난 업뎃

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 window.onload = function () {
+    $(document.querySelector(".footer")).load("../wrapper/footer_wrapper.html");
+}
+
+
+
+
+window.onload = function () {
     $(document.querySelector(".nav_bar")).load("../wrapper/nav_wrapper.html");
 }
+
