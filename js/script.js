681462611fc211df33da614d729c5ec3702fd93b
james1112 <jiwon1112kim@naver.com>
2022-04-13 11:52:25 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-window.onload = function () {
-    $(document.querySelector(".footer")).load("../wrapper/footer_wrapper.html");
-}
-
 
 
 
 window.onload = function () {
     $(document.querySelector(".nav_bar")).load("../wrapper/nav_wrapper.html");
+    $(document.querySelector(".footer")).load("../wrapper/footer_wrapper.html");
 }
 
