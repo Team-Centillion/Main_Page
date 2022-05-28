afe24d4714f4c21a939adc0421054ff0d31629cc
james1112 <jiwon1112kim@naver.com>
2022-05-28 18:59:14 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-window.onload = function () {
-    $(document.querySelector(".nav_bar")).load("../wrapper/nav_wrapper.html");
-    $(document.querySelector(".footer")).load("../wrapper/footer_wrapper.html");
-}
+$(document.querySelector(".nav_bar")).load("../wrapper/nav_wrapper.html");
+$(document.querySelector(".footer")).load("../wrapper/footer_wrapper.html");
+
 
